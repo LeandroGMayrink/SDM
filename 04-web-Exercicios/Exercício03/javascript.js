622ce30e9de03ec5846cbccfda4ed395acc184aa
Leandro Mayrink <32225957@ulife.com.br>
2023-10-07 13:42:07 -0300

    07/10

--- 04-web-Exercicios/Exercício03/javascript.js
+++ 04-web-Exercicios/Exercício03/javascript.js
@@ -1,8 +1,6 @@
 function verificarParImpar() {
-  // Obtenha o valor do input
   const numero = document.getElementById("numero").value;
 
-  // Verifique se o número é par ou ímpar
   if (numero % 2 === 0) {
     document.getElementById("resultado").textContent = `${numero} é um número PAR.`;
   } else {
